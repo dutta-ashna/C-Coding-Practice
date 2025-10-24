87f3640284db6c917ff2365a758899428be58212
Ashna Dutta <Ashna.27808@stu.upes.ac.in>
2025-10-24 10:51:04 +0530

    Added program to print the address of a variable using the address-of operator (&)

---
+++ print_variable_address.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10;   // Declare an integer variable
+    printf("Value of num: %d\n", num);          // Print the value
+    printf("Address of num: %p\n", &num);       // Print the address using &
+    return 0;
+}
